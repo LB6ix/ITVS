f1bb4980d6ae863fcd5b32cf3c39a16d044333d1
LB6ix <liudasshow@gmail.com>
2021-04-08 17:52:45 +0300

    Database connect added

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
-app.get ('/', (req, res) => res.send('API running'));
+app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
 
